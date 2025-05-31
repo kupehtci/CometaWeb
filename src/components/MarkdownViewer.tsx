088013e612b0ccff7c53d9c8578a8aaa592135bb
Daniel Laplana <daniel.laplana.gimeno@gmail.com>
2025-05-31 22:41:22 +0200

    Solved compilation error

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 import remarkGfm from 'remark-gfm';
 
 interface MarkdownViewerProps {
@@ -9,11 +9,14 @@ interface MarkdownViewerProps {
 }
 
 interface CodeProps {
-  node: any;
-  inline: boolean;
+  node?: unknown;
+  inline?: boolean;
+  style?: React.CSSProperties | null; 
   className?: string;
-  children: React.ReactNode;
+  children?: React.ReactNode;
 }
+
+
 
 const MarkdownViewer = ({ fileName }: MarkdownViewerProps) => {
   const [content, setContent] = useState<string>('');
@@ -60,14 +63,14 @@ const MarkdownViewer = ({ fileName }: MarkdownViewerProps) => {
       <ReactMarkdown
         remarkPlugins={[remarkGfm]}
         components={{
-          code({ node, inline, className, children, ...props }: CodeProps) {
+          code({ style, className, children, ...props } : CodeProps) {
             const match = /language-(\w+)/.exec(className || '');
-            return !inline && match ? (
+            return match ? (
               <SyntaxHighlighter
-                style={vscDarkPlus}
-                language={match[1]}
-                PreTag="div"
-                {...props}
+              language={match[1]}
+              PreTag="div"
+              {...props}
+              style={oneDark}
               >
                 {String(children).replace(/\n$/, '')}
               </SyntaxHighlighter>
